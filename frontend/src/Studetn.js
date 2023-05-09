9c4b72cd36447bdf4c5459263f784b86a89848c5
dinobotry <dino@robotry.co.kr>
2023-05-09 19:48:15 +0900

    프론트 수정

--- frontend/src/Studetn.js
+++ frontend/src/Studetn.js
@@ -1,32 +1,51 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Form from "./Form";
 function Studetn() {
+  const [students, setStudents] = useState([]);
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/")
+      .then(({ data }) => {
+        setStudents(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:8080/')
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-    }, [])
-
-    return (
-        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-            <div className='w-75 bg-white rounded'>
-                <button className='btn btn-success'>Add +</button>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-75 bg-white rounded">
+        <button
+          className="btn btn-success"
+          onClick={() => {
+            setVisible(!visible);
+          }}
+        >
+          Add +
+        </button>
+        {visible ? <Form close={() => setVisible(false)} /> : null}
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map((student, idx) => {
+              return (
+                <tr key={`s_${idx}`}>
+                  <td>{student.Name}</td>
+                  <td>{student.Email}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Studetn
+export default Studetn;
